6a16cf9aab7922b790ecf49bc2458dee1041341b
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-01-23 23:45:27 +0200

    Add name modal

---
+++ app/javascript/components/NameModal.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <b-modal id="name-modal" hide-header="true" hide-footer="true" centered :no-close-on-backdrop="true">
+    <div class="d-flex flex-column align-items-center text-center">
+      <h3 class="font-weight-light my-2">Who are you?</h3>
+      <input class="form-control my-2" placeholder="Lukin Skywalker">
+      <button class="btn btn-success btn-block text-uppercase my-2">let me in</button>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: 'NameModal'
+}
+</script>
--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Navbar/>
     <ProductsBoard/>
+    <NameModal/>
   </div>
 </template>
 
@@ -9,12 +10,17 @@
 
 import ProductsBoard from '../components/ProductsBoard.vue'
 import Navbar from '../components/Navbar.vue'
+import NameModal from '../components/NameModal.vue'
 
 export default {
   name: 'home',
   components: {
     ProductsBoard,
-    Navbar
+    Navbar,
+    NameModal
+  },
+  mounted () {
+    this.$bvModal.show('name-modal')
   }
 }
 </script>
