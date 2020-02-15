19efb57a0448216199b767c3c0113964245cae5b
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-02-15 14:38:51 +0200

    wip

---
+++ app/javascript/components/ProfileIcon.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="wrapper d-flex flex-column">
+    <img src="../packs/images/avatar.jpg" alt="avatar" class="h-100 rounded-circle">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileIcon'
+}
+</script>
+
+<style lang="scss" scoped>
+  .wrapper {
+    position: fixed;
+    top: 1rem;
+    right: 1rem;
+    height: 56px;
+  }
+</style>
--- app/javascript/views/Home.vue
+++ app/javascript/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <ProfileIcon/>
     <div class="row align-items-start">
       <ProductsBoard class="col col-lg-8"/>
       <Bucket class="col-md-4"/>
@@ -13,13 +14,15 @@
 import ProductsBoard from '../components/ProductsBoard.vue'
 import Bucket from '../components/Bucket.vue'
 import NameModal from '../components/NameModal.vue'
+import ProfileIcon from '../components/ProfileIcon.vue'
 
 export default {
   name: 'home',
   components: {
     ProductsBoard,
     NameModal,
-    Bucket
+    Bucket,
+    ProfileIcon
   },
   mounted () {
     if (this.$store.state.username) { return }
