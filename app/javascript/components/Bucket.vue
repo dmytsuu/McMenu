980e714433ccd448e4cbd44914c8c79f60862f70
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-02-15 12:23:20 +0200

    Restyle bucket

--- app/javascript/components/Bucket.vue
+++ app/javascript/components/Bucket.vue
@@ -1,20 +1,34 @@
 <template lang="html">
   <div class="container-fluid sticky-top">
-    <div class="row my-3 no-gutters flex-column align-items-center rounded-smooth shadow-lg">
+    <div class="bucket row my-3 no-gutters flex-column align-items-center rounded-smooth shadow-lg">
       <div class="col p-3">
-        <h5 class="text-center">Bucket</h5>
-        <table class="table">
-          <tbody>
-            <tr v-for="product in bucketProducts" :key="product.id" class="text-left table-row">
-              <td><img :src="product.image || placeholder" alt="thumbnail" class="thumbnail"></td>
-              <td>{{ product.name }}</td>
-              <td><span class="badge badge-success p-3">{{ product.quantity }}</span></td>
-              <td>{{ product.price * product.quantity }}₴</td>
-            </tr>
-          </tbody>
-        </table>
-        <button class="btn btn-success btn-block" @click="submit" :disabled="isSubmitDisabled">{{ submitButtonText }}</button>
-        <button class="btn btn-danger btn-block" @click="resetBucket">Reset</button>
+        <h5 class="text-center font-weight-bold">Your order</h5>
+        <div :class="{ 'd-none' : bucketProducts.length }" class="display-4 text-muted text-center">¯\_(ツ)_/¯</div>
+        <div :class="{ 'd-none' : !bucketProducts.length }" class="container-fluid product">
+          <div class="row py-2 no-gutters border-bottom" v-for="product in bucketProducts" :key="product.id">
+            <div class="col-2 font-weight-bold">
+              {{ product.quantity }}x
+            </div>
+            <div class="col">
+              {{ product.name }}
+            </div>
+            <div class="col-auto">
+              {{ product.price * product.quantity }} грн.
+            </div>
+          </div>
+          <div class="row py-3 justify-content-between font-weight-bold">
+            <div class="col-auto">
+              TOTAL PRICE
+            </div>
+            <div class="col-auto">
+              {{ this.totalPrice }} грн.
+            </div>
+          </div>
+        </div>
+        <div :class="{ 'd-none' : !bucketProducts.length }" class="row justify-content-center flex-wrap">
+          <button class="font-weight-bold btn btn-success w-75 rounded-pill mb-2" @click="submit" :disabled="isSubmitDisabled">{{ submitButtonText }}</button>
+          <button class="font-weight-bold btn btn-danger w-75 rounded-pill" @click="resetBucket">Reset</button>
+        </div>
       </div>
     </div>
   </div>
@@ -60,16 +74,13 @@ export default {
   },
   computed: {
     ...mapGetters(['bucketProducts', 'totalPrice']),
-    submitButtonText () { return this.isSubmitDisabled ? 'Submiting' : `Order ${this.totalPrice} ₴` },
+    submitButtonText () { return this.isSubmitDisabled ? 'Submiting' : 'Order' },
     username () { return this.$store.state.username }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  .table-row { vertical-align: center; }
-  .thumbnail {
-    width: 50px;
-    height: 50px;
-  }
+  // .bucket { min-height: 275px; }
+  .product { font-size: 17px; }
 </style>
