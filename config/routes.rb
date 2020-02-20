874ad3bb87a6e38427e1cb1037cbf15a41b4c067
Dmitry Sivoglaz <grayeyed16@gmail.com>
2020-02-20 18:23:18 +0200

    rubocop -a routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'products#index'
   post 'buckets/create'
   get 'products/index'
 
-
-
-
   post 'refresh', controller: :refresh, action: :create
   post 'signin', controller: :signin, action: :create
   post 'signup', controller: :signup, action: :create
   delete 'signin', controller: :signin, action: :destroy
-
 end
